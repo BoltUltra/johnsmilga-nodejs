5d7ae9c9e66aa8c5829382926c6a6fe70bb593c5
BoltUltra <mayowa.u.sunusi@gmail.com>
2023-07-31 13:31:06 +0100

    ...

--- app.js
+++ app.js
@@ -1,17 +1,17 @@
-const { readFile } = require("fs");
+let http = require("http");
+let fs = require("fs");
 
-const getText = (path) => {
-  return new Promise((resolve, reject) => {
-    readFile(path, "utf-8", (err, data) => {
-      if (err) {
-        reject(err);
-      } else {
-        resolve(data);
-      }
+http
+  .createServer((req, res) => {
+    // const text = fs.readFileSync("./content/big.txt", "utf-8");
+    // res.end(text);
+
+    const fileStream = fs.createReadStream("./content/big.txt", "utf-8");
+    fileStream.on("open", () => {
+      fileStream.pipe(res);
     });
-  });
-};
-
-getText(". /content/first.txt")
-  .then((result) => console.log(result))
-  .catch((err) => console.log(err));
+    fileStream.on("error", (err) => {
+      console.log(err);
+    });
+  })
+  .listen(5000);
